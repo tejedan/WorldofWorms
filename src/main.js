bf54547e0970881cdc80424aba639cd4750929b3
Nathan Tejeda <ntejeda@ucsc.edu>
2021-04-21 13:15:01 -0700

    added comments on main

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 
-//Nathan Tejeda, RPatrolMod(World of Worms), April 21, 2021, Took about 6 hours to complete
+//Nathan Tejeda, WorldofWorms, April 21, 2021, Took about 6 hours to complete
 // 60 points for Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi)
 // 20 points for Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points
 // 10 points for Create 4 new explosion SFX and randomize which one plays on impact
